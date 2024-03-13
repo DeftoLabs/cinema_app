2cb03720bdc6bf8faa2158df91d102f53ce3446e
DeftoLabs <deftolabs@gmail.com>
2024-03-13 15:03:30 -0400

    datasource + repository + isardatasource

---
+++ lib/infrastructure/datasource/isar_datasource.dart
@@ -0,0 +1,24 @@
+import 'package:cinema/domain/datasource/local_storage_datasource.dart';
+import 'package:cinema/domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource {  
+  
+  
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+}
