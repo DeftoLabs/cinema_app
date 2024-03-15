5cff3901fac93bb83ff1c26e517b38ebffae848b
DeftoLabs <deftolabs@gmail.com>
2024-03-15 12:47:29 -0400

    Favorite Screen

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,15 +1,54 @@
+import 'package:cinema/presentation/providers/providers.dart';
+import 'package:cinema/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+// init
+// solo las primeras 10
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  bool isLastPage = false;
+  bool isLoading = false;
+
+  @override
+
+  void initState() {
+    super.initState();
+    loadNextPage();
+  }
+
+  void loadNextPage() async {
+
+    if( isLoading || isLastPage) return;
+    isLoading = true;
+
+    final movies = await  ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    isLoading = false;
+
+    if(movies.isEmpty){
+      isLastPage = true;
+    }
+
+
+  }
+
+@override
   Widget build(BuildContext context) {
+
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
-      ),
-      body: const Center( child: Text('Favorites'),),
+      body: MovieMasonry (
+        loadNextPage: loadNextPage,
+        movies: favoriteMovies),
     );
   }
 }
